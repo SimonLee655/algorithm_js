7caee40777574a42f187dbad250431412a995e4b
simonlee <simonleecoding@gmail.com>
2019-11-25 23:35:19 +0800

    add priority queue

---
+++ src/priorityQueue.js
@@ -0,0 +1,40 @@
+function priorityQueue () {
+  this.q = [];
+}
+priorityQueue.prototype.queueElement = function (element, priority) {
+  this.element = element;
+  this.priority = priority;
+}
+priorityQueue.prototype.enqueue = function (element, priority) {
+  let qe = new this.queueElement(element, priority);
+  let added = false;
+  for (let index = 0; index < this.q.length; index++) {
+    if (qe.priority < this.q[index].priority) {
+      qe.splice(index, 0, qe);
+      added = true;
+      break;
+    }
+  }
+  if (!added) {
+    this.q.push(qe);
+  }
+}
+priorityQueue.prototype.isEmpty = function () {
+  return !this.q.length;
+}
+priorityQueue.prototype.dequeue = function () {
+  if (!this.isEmpty()) {
+    let e = this.q.shift();
+    return e.element;
+  }
+  return undefined;
+}
+priorityQueue.prototype.front = function () {
+  if (!this.isEmpty()) {
+    return this.q[0].element;
+  }
+  return undefined;
+}
+priorityQueue.prototype.size = function () {
+  return this.q.length;
+}
